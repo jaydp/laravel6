42669d2a00e66668e0d86e61c12223621687981e
Jaydeep <jaydp@users.noreply.github.com>
2020-04-07 12:28:14 +0930

    Create CRUD operation

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 6 CRUD Operation</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>List of Tasks</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('tasks.create') }}"> Create New Task</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>            
+            <th>Title</th>
+            <th>Description</th>
+            <th colspan="2">Action</th>
+        </tr>
+        @foreach ($tasks as $task)
+        <tr>            
+            <td>{{ $task->title }}</td>
+            <td>{{ $task->description }}</td>
+            <td><a class="btn btn-primary" href="{{ route('tasks.edit',$task->id) }}">Edit</a></td>
+			<td>
+                <form action="{{ route('tasks.destroy',$task->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+@endsection
